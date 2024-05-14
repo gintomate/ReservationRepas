d98a9176fe5182a5cdad2e910e8b80e86810f854
alexis irana <gintomate@gmail.com>
2024-05-14 06:10:53 +0400

    xs

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,23 +4,16 @@ namespace App\DataFixtures;
 
 use App\Entity\Promo;
 use App\Entity\Section;
-use App\Repository\PromoRepository;
-use App\Repository\SectionRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Provider\DateTime as DateTimeProvider;
 use Faker\Provider\en_US\Company;
-use Proxies\__CG__\App\Entity\User;
+
 
 class AppFixtures extends Fixture
 {
-    private $promoRepository;
 
-    public function __construct(PromoRepository $promoRepository)
-    {
-        $this->promoRepository = $promoRepository;
-    }
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('en_US');
@@ -29,14 +22,14 @@ class AppFixtures extends Fixture
 
         $jobWords = ['engineer', 'developer', 'manager', 'designer', 'analyst', 'consultant', 'specialist'];
 
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 8; $i++) {
             $section = new Section();
             $randomJobWord = $jobWords[array_rand($jobWords)];
             $section->setNomSection($randomJobWord);
             $section->setAbreviation($faker->word());
             $manager->persist($section);
 
-            for ($j = 0; $j < rand(1, 3); $j++) {
+            for ($j = 0; $j < rand(1, 2); $j++) {
                 $promo = new Promo();
                 $promo->setSection($section);
                 $promo->setDateDebut($faker->dateTimeBetween('-5 years', 'now'));
@@ -48,13 +41,7 @@ class AppFixtures extends Fixture
 
         $manager->flush();
 
-        // // Loop through every promo using promoRepo
-        // $promos = $this->promoRepository->findAll();
-        // foreach ($promos as $promo) {
-        //     $user = new User();
-        //     $user->setEmail($faker->email());
-        //     $user->getPassword("Test.123");
-        //     $user->setIdentifiant()
-        // }
+
     }
+
 }
---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\UserInfo;
+use App\Repository\PromoRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Provider\DateTime as DateTimeProvider;
+use Faker\Provider\en_US\Company;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixture extends Fixture
+{
+    private $promoRepository;
+    private $passwordHasher;
+    public function __construct(PromoRepository $promoRepository,  UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->promoRepository = $promoRepository;
+        $this->passwordHasher = $passwordHasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('en_US');
+        $faker->addProvider(new Company($faker));
+        $faker->addProvider(new DateTimeProvider($faker));
+        // Loop through every promo using promoRepo
+        $promos = $this->promoRepository->findAll();
+        foreach ($promos as $promoEn) {
+            for ($h = 0; $h < rand(4, 12); $h++) {
+                $identifiant = $this->generateUniqueIdentifier();
+
+                //User
+                $user = new User();
+                $hashedPassword = $this->passwordHasher->hashPassword(
+                    $user,
+                    "Test.123"
+                );
+                $user->setEmail($faker->email());
+                $user->setPassword($hashedPassword);
+                $user->setIdentifiant($identifiant);
+                $user->setRoles(['ROLE_STAGIAIRE']);
+                $manager->persist($user);
+                //UserInfo
+                $userInfo = new UserInfo;
+                $userInfo->setNom($faker->firstName());
+                $userInfo->setPrenom($faker->lastName());
+                $userInfo->setDateDeNaissance($faker->dateTimeBetween('-70 years', '-18 years'));
+                $userInfo->setUser($user);
+                $userInfo->setPromo($promoEn);
+                $userInfo->setMontantGlobal(0);
+                $manager->persist($userInfo);
+            }
+        }
+        $manager->flush();
+    }
+    private function generateUniqueIdentifier()
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $identifier = '';
+
+        // Generate a unique identifier that follows the regex pattern and is 7 characters long
+        do {
+            $identifier = substr(str_shuffle($characters), 0, 7);
+        } while (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/', $identifier));
+
+        return $identifier;
+    }
+}
